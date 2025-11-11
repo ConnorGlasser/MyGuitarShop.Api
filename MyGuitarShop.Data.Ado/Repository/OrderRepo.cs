4014211b1451b6199e225523edcd53b5a16f48cb
connorglasser <turborlr@gmail.com>
2025-11-10 22:28:21 -0800

    Created Order Entity and Repo classes

---
+++ MyGuitarShop.Data.Ado/Repository/OrderRepo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Azure.Identity;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Logging;
+using MyGuitarShop.Common.Interfaces;
+using MyGuitarShop.Data.Ado.Entities;
+using MyGuitarShop.Data.Ado.Factories;
+
+namespace MyGuitarShop.Data.Ado.Repository
+{
+    public class OrderRepo(
+        ILogger<OrderRepo> logger,
+        SqlConnectionFactory sqlConnectionFactory)
+        : IRepository<OrderEntity>
+    {
+        public async Task<IEnumerable<OrderEntity>> GetAllAsync()
+        {
+            // Create a list of orders
+            var orders = new List<OrderEntity>();
+
+            try
+            {
+                // Gets a connection to the sql database
+                await using var connection = await sqlConnectionFactory.OpenSqlConnectionAsync();
+
+                // creates an SQL command that uses said connection
+                await using var command = new SqlCommand("SELECT * FROM Customers", connection);
+
+                // creates an SQL Data reader that will read data from our sql tables
+                await using var reader = await command.ExecuteReaderAsync();
+
+                // Keep reading through every row 
+                while (await reader.ReadAsync())
+                {
+                    // create a new order var
+                    var order = new OrderEntity
+                    {
+                        // assign the info from the columns to each of the properties of the order
+                        OrderID = reader.GetInt32(reader.GetOrdinal("OrderID")),
+                        CustomerID = reader.IsDBNull(reader.GetOrdinal("CategoryID")) ? (int?)null : reader.GetInt32(reader.GetOrdinal("CategoryID")),
+                        OrderDate = reader.GetDateTime(reader.GetOrdinal("OrderDate")),
+                        ShipAmount = reader.GetDecimal(reader.GetOrdinal("ShipAmount")),
+                        TaxAmount = reader.GetDecimal(reader.GetOrdinal("TaxAmount")),
+                        ShipDate = reader.IsDBNull(reader.GetOrdinal("ShipDate")) ? (DateTime?)null : reader.GetDateTime(reader.GetOrdinal("ShipDate")),
+                        ShipAddressID = reader.GetInt32(reader.GetOrdinal("ShipAddressID")),
+                        CardType = reader.GetString(reader.GetOrdinal("CardType")),
+                        CardNumber = reader.GetString(reader.GetOrdinal("CardNumber")),
+                        CardExpires = reader.GetString(reader.GetOrdinal("CardExpires")),
+                        BillingAddressID = reader.GetInt32(reader.GetOrdinal("BillingAddressID"))
+                    };
+                    // add this order to the list
+                    orders.Add(order);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message, "Error retrieving categories list");
+            }
+
+            // Return the list of orders
+            // if there is an error, it will return a semi-complete list (everything up to the error)
+            return orders;
+        }
+    }
+}
