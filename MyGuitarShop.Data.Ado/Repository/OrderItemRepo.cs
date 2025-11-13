297a3f3edfd5de956ba2d5cb72def97d61b6df94
connorglasser <turborlr@gmail.com>
2025-11-12 19:56:24 -0800

    Modified IRepository to include Delete, FindByID, Insert, and Update commands. Implemented FindByID command to all repositories

--- MyGuitarShop.Data.Ado/Repository/OrderItemRepo.cs
+++ MyGuitarShop.Data.Ado/Repository/OrderItemRepo.cs
@@ -19,6 +19,54 @@ namespace MyGuitarShop.Data.Ado.Repository
         SqlConnectionFactory sqlConnectionFactory)
         : IRepository<OrderItemEntity>
     {
+        public Task<int> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<OrderItemEntity?> FindByIDAsync(int id)
+        {
+            // Create a orderItem var
+            OrderItemEntity orderItem = null;
+
+            try
+            {
+                // Gets a connection to the sql database
+                await using var connection = await sqlConnectionFactory.OpenSqlConnectionAsync();
+
+                // creates an SQL command that uses said connection
+                await using var command = new SqlCommand("SELECT * FROM OrderItems WHERE ItemID = @ItemID", connection);
+
+                // set the variable @ItemID with the id sent into this function
+                command.Parameters.AddWithValue("@ItemID", id);
+
+                // creates an SQL Data reader that will read data from our sql tables
+                await using var reader = await command.ExecuteReaderAsync();
+
+                // if the orderitem isn't null
+                if (await reader.ReadAsync())
+                {
+                    orderItem = new OrderItemEntity
+                    {
+                        // assign the info from the columns to each of the properties of the orderItem
+                        ItemID = reader.GetInt32(reader.GetOrdinal("ItemID")),
+                        OrderID = reader.GetInt32(reader.GetOrdinal("OrderID")),
+                        ProductID = reader.GetInt32(reader.GetOrdinal("ProductID")),
+                        ItemPrice = reader.GetDecimal(reader.GetOrdinal("ItemPrice")),
+                        DiscountAmount = reader.GetDecimal(reader.GetOrdinal("DiscountAmount")),
+                        Quantity = reader.GetInt32(reader.GetOrdinal("Quantity"))
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message, $"Error finding orderItem {id} by ID");
+            }
+
+            // return the orderItem, or null if an error occured
+            return orderItem;
+        }
+
         public async Task<IEnumerable<OrderItemEntity>> GetAllAsync()
         {
             // Create a list of orderItems
@@ -61,6 +109,16 @@ namespace MyGuitarShop.Data.Ado.Repository
             // Return the list of orderItems
             // if there is an error, it will return a semi-complete list (everything up to the error)
             return orderItems;
+        }
+
+        public Task<int> InsertAsync(OrderItemEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> UpdateAsync(int id, OrderItemEntity entity)
+        {
+            throw new NotImplementedException();
         }
     }
 }
